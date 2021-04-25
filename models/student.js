80ba295334402a2c675eaaa05542e5c47085d730
raphaelkx <raphaelkx@gmail.com>
2021-04-24 21:18:10 -0300

    Adiciona comentarios modelo student

--- models/student.js
+++ models/student.js
@@ -17,6 +17,7 @@ const studentSchema = mongoose.Schema({
     type: Number,
     required: true,
     //Valida se a nota inserida e' menor que zero
+    //valida a validação do professor
     validate(value) {
       if (value < 0) throw new Error('Valor negativo para nota');
     },
